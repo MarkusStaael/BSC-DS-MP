7e8766857d77cbe394c69f6f85195812d4c5df87
Patrick <patrickshen.ps@gmail.com>
2026-03-12 16:36:26 +0100

    stuff

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ bool printResult = false;
 bool toFile = false;
 string[] files = { "test.gr", "30z50.gr", "heuristic_001.gr", "bremen_subgraph_300.gr" };
 int target = 2;
-int timelimit = 20; // seconds
+int timelimit = 300; // seconds
 
 string targetTest = files[target];
 string projroot = Path.Combine(AppContext.BaseDirectory, "..", "..", "..");
