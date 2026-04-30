818f2f0360acc4dd9497cefda90c53f008262585
Markus Sværke Staael <markus@staael.com>
2026-04-30 15:27:45 +0200

    Prepare for graph reduction + remove unused files

---
+++ Util/GraphReducer/GraphReducer.cs
@@ -0,0 +1,19 @@
+﻿using BSC_DS_MP.DataStructures.Graph;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSC_DS_MP.Util; 
+public class GraphReducer : IGraphReducer {
+
+    public GraphReducer() {
+    }
+
+    public (IGraph, int[],int) Reduce(IGraph graph) {
+        throw new NotImplementedException();
+    }
+    
+    public IGraph Reconstruct(IGraph reducedGraph) {
+        throw new NotImplementedException();
+    }
+}
---
+++ Util/GraphReducer/IGraphReducer.cs
@@ -0,0 +1,10 @@
+﻿using BSC_DS_MP.DataStructures.Graph;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BSC_DS_MP.Util; 
+public interface IGraphReducer {
+    public (IGraph, int[],int) Reduce(IGraph graph);
+    public IGraph Reconstruct(IGraph reducedGraph);
+}
